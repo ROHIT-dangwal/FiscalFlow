e2f9ba4dd5f3a60741fd3b9a4e7d4e726ac0b1a4
manankasturia <manankasturia5@gmail.com>
2026-04-26 02:25:43 +0530

    ssl config

--- FiscalFlow-Backend/server.js
+++ FiscalFlow-Backend/server.js
@@ -43,6 +43,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.DB_HOST
+    ? { require: true, rejectUnauthorized: false }
+    : false,
 });
 
 pool.connect((err, client, release) => {
